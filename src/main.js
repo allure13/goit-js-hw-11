7a8a8a034dfe8458f9f7ed5ae174be68a1c8a217
allure13 <igor.cheplyaka@gmail.com>
2023-12-29 17:52:36 +0200

    додав форму та кнопку

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '41530173 - f95b78bdec41263a85620f647';
