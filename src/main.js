615b6d84d56ca58d91c4abf47c20faf34e133762
allure13 <igor.cheplyaka@gmail.com>
2023-12-29 18:55:41 +0200

    зробив толково форму, кнопку, почав писати js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '41530173 - f95b78bdec41263a85620f647';
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '41530173-f95b78bdec41263a85620f647';
+
+const searchForm = document.querySelector('.form');
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
